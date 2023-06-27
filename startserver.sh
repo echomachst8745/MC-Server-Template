cc223a030bcd86f90ad232d7a06c52fde1f95235
EatHose8745 <joel.crann22@hotmail.co.uk>
2023-06-27 21:25:54 +0100

    updated startup script to create backup directories

--- startserver.sh
+++ startserver.sh
@@ -8,9 +8,10 @@ fi
 
 SAVEINTERVAL=600
 
-while getopts 's:' flag; do
+while getopts 's:d:w:' flag; do
   case "$flag" in
     s) SAVEINTERVAL=$OPTARG ;;
+    d) 
   esac
 done
 
@@ -20,11 +21,21 @@ echo "If this is your first time running you will have to agree to the EULA and 
 screen -dmS mc-server
 screen -dmS autosave-mc-server
 
-screen -dmS backup-daily-mc-server
-screen -dmS backup-weekly-mc-server
-
 screen -S mc-server -X stuff 'cd server-files && bash ../server-scripts/startupscript.sh\n'
 screen -S autosave-mc-server -X stuff 'cd server-scripts && bash autosaveserverscript.sh -n '$SAVEINTERVAL'\n'
+
+if [ ! -d "server-backups" ]; then
+  mkdir "server-backups"
+fi
+if [ ! -d "server-backups/daily-backups" ]; then
+  mkdir "server-backups/daily-backups"
+fi
+if [ ! -d "server-backups/weekly-backups" ]; then
+  mkdir "server-backups/weekly-backups"
+fi
+
+screen -dmS backup-daily-mc-server
+screen -dmS backup-weekly-mc-server
 
 screen -S backup-daily-mc-server -X stuff 'cd server-scripts && watch -n 86400 bash makedailybackup.sh\n'
 screen -S backup-weekly-mc-server -X stuff 'cd server-scripts && watch -n 604800 bash makeweeklybackup.sh\n'
